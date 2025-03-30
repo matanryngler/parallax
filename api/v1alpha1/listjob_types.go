724b3ff2192d6777ba4baf1fc9f1bdbec10b6e4a
Matan Ryngler <matanryngler@gmail.com>
2025-03-30 17:38:38 +0300

    update operator behaviour and add flexibility in configuration

--- api/v1alpha1/listjob_types.go
+++ api/v1alpha1/listjob_types.go
@@ -1,36 +1,24 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type JobTemplateSpec struct {
-	Image   string   `json:"image"`
-	Command []string `json:"command"`
-	EnvName string   `json:"envName"`
+	Image     string                      `json:"image"`
+	Command   []string                    `json:"command"`
+	EnvName   string                      `json:"envName"`
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 type ListJobSpec struct {
-	ListSourceRef string          `json:"listSourceRef,omitempty"`
-	StaticList    []string        `json:"staticList,omitempty"`
-	Parallelism   int32           `json:"parallelism"`
-	Template      JobTemplateSpec `json:"template"`
+	ListSourceRef           string           `json:"listSourceRef,omitempty"`
+	StaticList              []string         `json:"staticList,omitempty"`
+	Parallelism             int32            `json:"parallelism"`
+	Template                JobTemplateSpec  `json:"template"`
+	TTLSecondsAfterFinished *int32           `json:"ttlSecondsAfterFinished,omitempty"`
+	DeleteAfter             *metav1.Duration `json:"deleteAfter,omitempty"`
 }
 
 type ListJobStatus struct {
